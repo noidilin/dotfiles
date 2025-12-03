7978c13f92fae7922487990a9e45f8773f0ef2c6
noidilin <linganinja.0120@gmail.com>
2025-12-04 01:36:45 +0800

    fix(pwsh): update pwsh profile to sync with the behavior in nushell

--- home/dot_config/pwsh/profile.ps1
+++ home/dot_config/pwsh/profile.ps1
@@ -1,9 +1,8 @@
 $env:EDITOR = "nvim"
-$env:VISUAL = "code"
 $env:PAGER = "delta"
 
 $env:CARAPACE_BRIDGES = "zsh,fish,bash,inshellisense" # optional
-$env:ANTHROPIC_API_KEY = Get-Content -Path "$HOME\.config\api\ANTHROPIC_API_KEY"
+$env:CC = 'gcc'
 
 # dev path
 $env:PNPM_HOME = "$HOME\.dev\pnpm"
@@ -12,7 +11,14 @@ $env:BUN_INSTALL_BIN = "$HOME\.dev\bun\bin"
 $env:BUN_INSTALL_CACHE_DIR = "$HOME\.dev\bun\install\cache"
 $env:CARGO_HOME = "$HOME\.dev\cargo"
 $env:RUSTUP_HOME = "$HOME\.dev\rustup"
+## UV configuration - force all storage to follow XDG-like structure on Windows
+$env:UV_CONFIG_FILE =  "$HOME\.config\uv\uv.toml"
+$env:UV_CACHE_DIR = "$HOME\.dev\uv\cache"
+$env:UV_PYTHON_INSTALL_DIR = "$HOME\.dev\uv\python"
+$env:UV_TOOL_DIR = "$HOME\.dev\uv\tools"
+$env:UV_TOOL_BIN_DIR = "$HOME\.dev\uv\bin"
 
+$env:PATH += ";$HOME\.dev\uv\bin"
 $env:PATH += ";$HOME\.dev\pnpm"
 $env:PATH += ";$HOME\.dev\bun\bin"
 
@@ -21,7 +27,11 @@ $env:EZA_CONFIG_DIR = "$HOME\.config\eza"
 $env:BAT_CONFIG_DIR = "$HOME\.config\bat"
 $env:YAZI_CONFIG_HOME = "$HOME\.config\yazi"
 
-Import-Module posh-git
+# key
+$env:ANTHROPIC_API_KEY = Get-Content -Path "$HOME\.config\api\ANTHROPIC_API_KEY"
+$env:TAVILY_API_KEY = Get-Content -Path "$HOME\.config\api\TAVILY_API_KEY"
+$env:OPENAI_API_KEY = Get-Content -Path "$HOME\.config\api\OPENAI_API_KEY"
+
 Import-Module PSReadLine
 
 . "$HOME\.config\pwsh\scripts\PSReadLine.ps1"
@@ -44,5 +54,3 @@ if (Get-Command "chezmoi" -ErrorAction SilentlyContinue) {
 if (Get-Command "carapace" -ErrorAction SilentlyContinue) {
   Invoke-Expression (& { (carapace _carapace | Out-String) })
 }
-
-# fastfetch
--- home/dot_config/pwsh/scripts/commands.ps1
+++ home/dot_config/pwsh/scripts/commands.ps1
@@ -1,77 +1,10 @@
+function Reload-Profile {
+  . $PROFILE
+}
+
 Set-Alias -Name rl -Value Reload-Profile
+
 if (Get-Command lazygit) { Set-Alias -Name lg -Value lazygit }
-
-function Replace-MochaPalette {
-    <#
-    .SYNOPSIS
-        A function to replace catppuccin mocha color palette
-    .DESCRIPTION
-        A function to replace catppuccin mocha color palette. A file path must be include to start replacing the hex color code inside it.
-        Use switch `-stylus` when replacing stylus userstyles.
-    #>
-    param (
-        [string]$filePath,
-        [switch]$stylus
-    )
-
-    # Get the content from the file
-    $content = Get-Content -Path $filePath -Raw
-
-    # Apply the mandatory replacements
-    ## The backtick (`) is used to indicate that the command continues on the next line, making the script easieer to read
-    $updatedContent = $content `
-        -replace '#f5c2e7', '#9f9fbd' `
-        -replace '#cba6f7', '#797994' `
-        -replace '#f38ba8', '#b07878' `
-        -replace '#eba0ac', '#cc9393' `
-        -replace '#fab387', '#d6caab' `
-        -replace '#f9e2af', '#ebd6b7' `
-        -replace '#a6e3a1', '#778777' `
-        -replace '#94e2d5', '#9bb09b' `
-        -replace '#89dceb', '#92b3b3' `
-        -replace '#74c7ec', '#769494' `
-        -replace '#89b4fa', '#7d96ad' `
-        -replace '#b4befe', '#9db2cf' `
-        -replace '#cdd6f4', '#b3b3b3' `
-        -replace '#bac2de', '#9d9d9d' `
-        -replace '#a6adc8', '#878787' `
-        -replace '#9399b2', '#707070' `
-        -replace '#7f849c', '#5d5d5d' `
-        -replace '#6c7086', '#4e4e4e' `
-        -replace '#585b70', '#414141' `
-        -replace '#45475a', '#353535' `
-        -replace '#313244', '#2a2a2a' `
-        -replace '#1e1e2e', '#1e1e1e' `
-        -replace '#181825', '#191919' `
-        -replace '#11111b', '#151515'
-
-    # Conditionally apply the color replacement based on the $stylus switch
-    if ($stylus) {
-        $updatedContent = $updatedContent `
-            -replace '"updateInterval":24,"updateOnlyEnabled":true', '"updateInterval":0,"updateOnlyEnabled":true' `
-            -replace '"name":"accentColor","value":null', '"name":"accentColor","value":"rosewater"' `
-            -replace '#f5e0dc', '#c0baad' `
-            -replace '#f2cdcd', '#a69f91'
-    } else {
-        $updatedContent = $updatedContent `
-            -replace '#f5e0dc', '#dcb5a5' `
-            -replace '#f2cdcd', '#c8a492'
-    }
-
-    # Write the updated content back to the file
-    Set-Content -Path $filePath -Value $updatedContent
-}
-
-function Update-Stylus { 
-    <#
-    .SYNOPSIS
-        A function that update catppuccin stylus userstyles
-    #>
-    $url = "https://github.com/catppuccin/userstyles/releases/download/all-userstyles-export/import.json"
-    $file = "$env:USERPROFILE\.local\etc\stylus\color-fatigue.json"
-    Invoke-WebRequest -Uri $url -OutFile $file # download file from web, and save to config path
-    Replace-MochaPalette -filePath $file -stylus # update file with stylus switch on
-}
 
 function yz {
     <#
@@ -138,47 +71,3 @@ function Clean-All {
   Delete-TempData
   Run-DiskCleanUp
 }
-
-function Reload-Profile {
-  . $PROFILE
-}
-
-# App manage
-<#
-function _List-ScoopApps {
-  $apps = $(scoop list | Select-Object -ExpandProperty "Name").Split("\n")
-  $apps = $apps[1..($apps.Length - 1)]
-  return $apps
-}
-
-function _Select-Apps {
-  param (
-    [string[]] $apps
-  )
-  $apps = $apps | fzf --prompt="Select Apps  " --height=~80% --layout=reverse --border --cycle --margin="2,20" --padding=1 --multi
-  return $apps
-}
-
-function Upgrade-ScoopApps {
-  $apps_set = New-Object System.Collections.Generic.HashSet[[String]]
-  $installed_apps = _List-ScoopApps
-  foreach ($app in _Select-Apps $installed_apps) {
-    $apps_set.Add($app) >$null
-  }
-  if ($apps_set.Length) {
-    $apps_string = ($apps_set -split ",")
-    scoop update $apps_string
-  } else {
-    Write-Host "No app was selected to update"
-  }
-}
-
-function Uninstall-ScoopApps {
-  $apps = _Select-Apps $(_List-ScoopApps)
-  if ($apps.Length -eq 0) {
-    Write-Host "No app was selected"!
-    return 
-  }
-  scoop uninstall $apps
-}
-#>
